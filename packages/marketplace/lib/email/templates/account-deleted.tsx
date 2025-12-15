3e02a4a9fe84db1f69ac9f3a17d6f16ee14fd7cb
Aigars Grenins <aigars.grenins@gmail.com>
2025-12-15 22:18:17 +0200

    fix(build): replace @react-email/components with individual packages

--- packages/marketplace/lib/email/templates/account-deleted.tsx
+++ packages/marketplace/lib/email/templates/account-deleted.tsx
@@ -1,17 +1,14 @@
-import {
-    Body,
-    Button,
-    Container,
-    Head,
-    Heading,
-    Html,
-    Preview,
-    Section,
-    Text,
-    Hr,
-} from '@react-email/components';
+import { Body } from '@react-email/body';
+import { Button } from '@react-email/button';
+import { Container } from '@react-email/container';
+import { Head } from '@react-email/head';
+import { Heading } from '@react-email/heading';
+import { Html } from '@react-email/html';
+import { Preview } from '@react-email/preview';
+import { Section } from '@react-email/section';
+import { Text } from '@react-email/text';
+import { Hr } from '@react-email/hr';
 import * as React from 'react';
-
 
 interface AccountDeletedEmailProps {
     userName: string;
